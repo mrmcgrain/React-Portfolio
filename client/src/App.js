e9a83abd1dc286ef080904f244e9d4157a7c577c
mrmcgain <michael.r.mcgrain@gmail.com>
2023-12-19 16:13:25 -0700

    RP-329 contact Nav info

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
+import Nav from "./components/Nav/Nav"
+import "./global.css"
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"></header>
 
-      </header>
+      <body>
+
+        <div id="leftNav">
+          <Nav />
+        </div>
+
+
+
+
+        <div id="rightBody">
+
+        </div>
+
+
+
+
+      </body>
+
+
+
+
+
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Routes, Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<App />} />
+        {/* <Route path="/" element={<App />} />
+        <Route path="/" element={<App />} />
+        <Route path="/" element={<App />} /> */}
+
+
+
+      </Routes>
+      {/* <App /> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
