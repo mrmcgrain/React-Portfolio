a8d4264033aa9c65f682bb066c6c80a5e61a3e2b
mrmcgain <michael.r.mcgrain@gmail.com>
2023-12-20 11:38:19 -0700

    Built template for 1st 3 sections

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,8 +1,87 @@
+import './CSS/Projects.css'
+
 function Projects() {
 
     return (
         <div id="Projects">
-            Projects
+            <h3>Projects</h3>
+
+            <div class="grid-container">
+                <div class="grid-item">
+
+                    <div id="image">where</div>
+                    <div id="buttons">
+
+                        <div className='button'>live</div>
+                        <div className='button'>code</div>
+
+                    </div>
+
+                </div>
+
+                <div class="grid-item">
+
+                    <div id="image">where</div>
+                    <div id="buttons">
+
+                        <div className='button'>live</div>
+                        <div className='button'>code</div>
+
+                    </div>
+
+                </div>
+
+                <div class="grid-item">
+
+                    <div id="image">where</div>
+                    <div id="buttons">
+
+                        <div className='button'>live</div>
+                        <div className='button'>code</div>
+
+                    </div>
+
+                </div>
+
+                <div class="grid-item">
+
+                    <div id="image">where</div>
+                    <div id="buttons">
+
+                        <div className='button'>live</div>
+                        <div className='button'>code</div>
+
+                    </div>
+
+                </div>
+
+                <div class="grid-item">
+
+                    <div id="image">where</div>
+                    <div id="buttons">
+
+                        <div className='button'>live</div>
+                        <div className='button'>code</div>
+
+                    </div>
+
+                </div>
+
+                <div class="grid-item">
+
+                    <div id="image">where</div>
+                    <div id="buttons">
+
+                        <div className='button'>live</div>
+                        <div className='button'>code</div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
         </div>
 
     );
--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,8 +1,98 @@
+import "./CSS/Skills.css"
+import socket from "./assests/icons/socket.png"
+import bcrypt from './assests/icons/bcrypr.jpeg'
+import code from './assests/icons/code2.png'
+import express from './assests/icons/express.png'
+import gsuite from './assests/icons/g-suite.png'
+import html from './assests/icons/html.png'
+import JWT from './assests/icons/JWT.png'
+import mongoDB from './assests/icons/mongoDB.png'
+import node from './assests/icons/nodejs.png'
+import scrum from './assests/icons/scrum.png'
+import react from './assests/icons/react.png'
+import JS from './assests/icons/java-script.png'
+
+<div
+class="grid-item-skill"
+style={{ backgroundImage: `url(${scrum})` }}
+>
+</div>
 function Skills() {
 
     return (
         <div id="Skills">
-            Skills
+            <h3>Skills</h3>
+            {/* {console.log("WTF", scrum)} */}
+            <div class="grid-container-skill">
+            
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${JS})` }}
+                >
+                </div>
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${react})` }}
+                >
+                </div>
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${express})` }}
+                >
+                </div>
+            
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${socket})` }}
+                >
+                </div>
+            
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${scrum})` }}
+                >
+                </div>
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${bcrypt})` }}
+                >
+                </div>
+            
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${html})` }}
+                >
+                </div>
+            
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${JWT})` }}
+                >
+                </div>
+            
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${mongoDB})` }}
+                >
+                </div>
+            
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${node})` }}
+                >
+                </div>
+                <div
+                    class="grid-item-skill"
+                    style={{ backgroundImage: `url(${gsuite})` }}
+                >
+                </div>
+
+
+
+
+
+
+            </div>
         </div>
 
     );
